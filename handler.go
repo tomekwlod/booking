0b1e649192c611954205da51fd2f0bb8d3f19c7e
Tomasz Wlodarczyk <tomek.wlod@gmail.com>
2021-05-24 22:45:38 +0100

    streaming

--- handler.go
+++ handler.go
@@ -1,8 +1,14 @@
 package main
 
+// https://stackoverflow.com/questions/34177137/stream-file-upload-to-aws-s3-using-go
+// https://medium.com/spankie/upload-images-to-aws-s3-bucket-in-a-golang-web-application-2612bea70dd8
+// https://docs.digitalocean.com/products/spaces/resources/s3-sdk-examples/
+// https://medium.com/@owlwalks/dont-parse-everything-from-client-multipart-post-golang-9280d23cd4ad
+// https://codepen.io/PerfectIsShit/pen/zogMXP -- progress bar done on a frontend
+
 import (
-	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"mime/multipart"
 	"net/http"
@@ -12,7 +18,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/google/uuid"
 	_ "github.com/jackc/pgx/stdlib"
 	"github.com/tomekwlod/utils/env"
@@ -48,82 +54,88 @@ func jsonHandler(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, j, http.StatusOK)
 }
 
-// UploadFileToS3 saves a file to aws bucket and returns the url to // the file and an error if there's any
-func UploadFileToS3(s *session.Session, file multipart.File, fileHeader *multipart.FileHeader) (string, error) {
-	// get the file size and read
-	// the file content into a buffer
-	size := fileHeader.Size
-	buffer := make([]byte, size)
-	file.Read(buffer)
-
-	fileext := filepath.Ext(fileHeader.Filename)
-	filename := fileHeader.Filename[0 : len(fileHeader.Filename)-len(fileext)]
-
-	// create a unique file name for the file
-	tempFileName := path.Join(
-		env.Env("DO_S3_DIR", ""),
-		fmt.Sprintf("%s_%s%s", filename, uuid.New().String(), filepath.Ext(fileHeader.Filename)),
-	)
-
-	// config settings: this is where you choose the bucket,
-	// filename, content-type and storage class of the file
-	// you're uploading
-	_, err := s3.New(s).PutObject(&s3.PutObjectInput{
-		Bucket: aws.String(env.Env("DO_S3_BUCKET", "")),
-		Key:    aws.String(tempFileName),
-		// ACL:                  aws.String("public-read"), // could be private if you want it to be access by only authorized users
-		Body:                 bytes.NewReader(buffer),
-		ContentLength:        aws.Int64(int64(size)),
-		ContentType:          aws.String(http.DetectContentType(buffer)),
-		ContentDisposition:   aws.String("attachment"),
-		ServerSideEncryption: aws.String("AES256"),
-		StorageClass:         aws.String("INTELLIGENT_TIERING"),
-	})
-	if err != nil {
-		return "", err
-	}
-
-	return tempFileName, err
-}
-
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
-	maxSize := int64(1024000 * 25) // allow only 1MB of file size *25
+	// function body of a http.HandlerFunc
+	r.Body = http.MaxBytesReader(w, r.Body, 25<<20+1024) // 25Mb
+	reader, err := r.MultipartReader()
 
-	err := r.ParseMultipartForm(maxSize)
 	if err != nil {
-		log.Println(err)
-		fmt.Fprintf(w, "Image too large. Max Size: %v", maxSize)
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	file, fileHeader, err := r.FormFile("profile_picture")
-	if err != nil {
-		log.Println(err)
-		fmt.Fprintf(w, "Could not get uploaded file")
+	// parse file field
+	mp, err := reader.NextPart()
+	if err != nil && err != io.EOF {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer file.Close()
+	if mp.FormName() != "profile_picture" {
+		http.Error(w, "profile_picture is expected", http.StatusBadRequest)
+		return
+	}
 
-	// create an AWS session which can be
-	// reused if we're uploading many files
-	s, err := session.NewSession(&aws.Config{
+	// buf := bufio.NewReader(mp)
+	// sniff, _ := buf.Peek(512)
+	// contentType := http.DetectContentType(sniff)
+	// if contentType != "application/zip" {
+	// 	http.Error(w, "file type not allowed", http.StatusBadRequest)
+	// 	return
+	// }
+
+	awsConfig := &aws.Config{
+		Endpoint: aws.String(env.Env("DO_S3_ENDPOINT", "https://nyc3.digitaloceanspaces.com")),
+		Region:   aws.String(env.Env("DO_S3_REGION", "us-east-1")),
+
 		Credentials: credentials.NewStaticCredentials(
 			env.Env("DO_S3_KEY", ""),
 			env.Env("DO_S3_SECRET", ""),
 			"",
 		),
-		Endpoint: aws.String(env.Env("DO_S3_ENDPOINT", "https://nyc3.digitaloceanspaces.com")),
-		Region:   aws.String(env.Env("DO_S3_REGION", "us-east-1")),
+	}
+
+	// The session the S3 Uploader will use
+	sess := session.Must(session.NewSession(awsConfig))
+
+	fileName, err := uploadFileToS3(sess, mp)
+
+	if err != nil {
+		fmt.Fprintf(w, "Could not upload file %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Image uploaded successfully to: %v", fileName)
+}
+
+// uploadFileToS3 saves a file to aws bucket and returns the url to // the file and an error if there's any
+func uploadFileToS3(sess *session.Session, file *multipart.Part) (string, error) {
+	// Create an uploader with the session and custom options
+	uploader := s3manager.NewUploader(sess, func(u *s3manager.Uploader) {
+		u.PartSize = 5 * 1024 * 1024 // The minimum/default allowed part size is 5MB
+		u.Concurrency = 2            // default is 5
 	})
+
+	fileext := filepath.Ext(file.FileName())
+	filename := file.FileName()[0 : len(file.FileName())-len(fileext)]
+
+	// create a unique file name for the file
+	tempFileName := path.Join(
+		env.Env("DO_S3_DIR", ""),
+		fmt.Sprintf("%s_%s%s", filename, uuid.New().String(), filepath.Ext(file.FileName())),
+	)
+
+	// Upload the file to S3.
+	result, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(env.Env("DO_S3_BUCKET", "booking")),
+		Key:    aws.String(tempFileName),
+		Body:   file,
+	})
+
+	// in case it fails to upload
 	if err != nil {
-		fmt.Fprintf(w, "Could not upload file")
+		return "", err
 	}
 
-	fileName, err := UploadFileToS3(s, file, fileHeader)
-	if err != nil {
-		fmt.Fprintf(w, "Could not upload file")
-	}
-
-	fmt.Fprintf(w, "Image uploaded successfully: %v", fileName)
+	return result.Location, nil
 }
